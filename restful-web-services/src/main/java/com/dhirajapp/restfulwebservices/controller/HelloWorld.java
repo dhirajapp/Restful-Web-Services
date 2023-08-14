b7771c9c15c3ec6e808ec0ae7b8c9900906c1e2e
dhirajapp <dhirajkumar31298@gmail.com>
2023-08-14 18:23:47 +0530

    Enhance hello restful webservices implemented

--- restful-web-services/src/main/java/com/dhirajapp/restfulwebservices/controller/HelloWorld.java
+++ restful-web-services/src/main/java/com/dhirajapp/restfulwebservices/controller/HelloWorld.java
@@ -1,14 +1,19 @@
 package com.dhirajapp.restfulwebservices.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
   
-	@RequestMapping(method=RequestMethod.GET,path="/hello")
+	@GetMapping(path="/hello")
 	public String helloWorld() {
-		return "Hello Dhiraj";
+		return "Hello World";
+	}
+	
+	@GetMapping(path="/hello-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean ("Hello World");
+		
 	}
 }
