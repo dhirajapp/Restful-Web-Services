cfb58dcb623462b38e4b58da00be8400f2b82898
dhirajapp <dhirajkumar31298@gmail.com>
2023-08-15 23:53:05 +0530

    Users rest api implement

--- restful-web-services/src/main/java/com/dhirajapp/restfulwebservices/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/dhirajapp/restfulwebservices/user/UserDaoService.java
@@ -10,17 +10,18 @@ import org.springframework.stereotype.Component;
 public class UserDaoService {
     // JPA/Hibernate -> Database
 	// UserDaoService  -> static List
-	
+	private static int userCount=0;
 	private static List<User> users=new ArrayList<>();
 	
 	static {
-		users.add(new User(01,"Dhiraj Kumar",LocalDate.now().minusYears(24)));
-		users.add(new User(02,"Sonu Kumar ",LocalDate.now().minusYears(32)));
-		users.add(new User(03,"Atherv Singh",LocalDate.now().minusYears(05)));
-		users.add(new User(04,"Neeraj Kumar",LocalDate.now().minusYears(18)));
-		users.add(new User(05,"Rajesh Kumar",LocalDate.now().minusYears(34)));
-		users.add(new User(06,"Suresh Kumar",LocalDate.now().minusYears(27)));
+		users.add(new User(++userCount,"Dhiraj Kumar",LocalDate.now().minusYears(24)));
+		users.add(new User(++userCount,"Sonu Kumar ",LocalDate.now().minusYears(32)));
+		users.add(new User(++userCount,"Atherv Singh",LocalDate.now().minusYears(05)));
+		users.add(new User(++userCount,"Neeraj Kumar",LocalDate.now().minusYears(18)));
+		users.add(new User(++userCount,"Rajesh Kumar",LocalDate.now().minusYears(34)));
+		users.add(new User(++userCount,"Suresh Kumar",LocalDate.now().minusYears(27)));
 	}
+	
 	
 	public List<User> findAll(){
 		return users;
@@ -30,4 +31,12 @@ public class UserDaoService {
 		Predicate<? super User> predicate = user -> user.getId().equals(id); 
 		return users.stream().filter(predicate).findFirst().get();
 	}
+	
+	public User save(User user) {
+		user.setId(++userCount);
+		users.add(user);
+		return user;
+	}
+	
+	
 }
--- restful-web-services/src/main/java/com/dhirajapp/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/dhirajapp/restfulwebservices/user/UserResource.java
@@ -1,10 +1,15 @@
 package com.dhirajapp.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.*;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -25,4 +30,11 @@ public class UserResource {
 		return service.findOne(id);
 	}
 	
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user){
+		
+		User savedUser=service.save(user);
+		URI location=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
+	}
 }
