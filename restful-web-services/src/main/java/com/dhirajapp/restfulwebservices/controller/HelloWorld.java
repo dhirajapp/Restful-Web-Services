aa95a2a845c98c6844973cf0b0426353837ca6ef
dhirajapp <dhirajkumar31298@gmail.com>
2023-08-15 15:48:36 +0530

    implement path parameter

--- restful-web-services/src/main/java/com/dhirajapp/restfulwebservices/controller/HelloWorld.java
+++ restful-web-services/src/main/java/com/dhirajapp/restfulwebservices/controller/HelloWorld.java
@@ -1,19 +1,27 @@
 package com.dhirajapp.restfulwebservices.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
-  
-	@GetMapping(path="/hello")
+
+	@GetMapping(path = "/hello")
 	public String helloWorld() {
 		return "Hello World";
 	}
-	
-	@GetMapping(path="/hello-bean")
+
+	@GetMapping(path = "/hello-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean ("Hello World");
-		
+		return new HelloWorldBean("Hello World");
+
 	}
+	
+	@GetMapping(path = "/hello-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World %s" , name));
+
+	}
+	
 }
