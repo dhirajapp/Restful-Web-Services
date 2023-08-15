5cfdfece125d1a28d6b8ccfee4c1cafe9155a251
dhirajapp <dhirajkumar31298@gmail.com>
2023-08-16 00:00:17 +0530

    Users rest api implement

--- restful-web-services/src/main/java/com/dhirajapp/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/dhirajapp/restfulwebservices/user/UserResource.java
@@ -27,7 +27,12 @@ public class UserResource {
 	
 	@GetMapping("/users/{id}")
 	public User getUser(@PathVariable int id){
-		return service.findOne(id);
+		
+	   User user=service.findOne(id);
+	   if(user==null)
+		   throw new UserNotFoundException;
+	   
+	   return user;
 	}
 	
 	@PostMapping("/users")
