c84137906c03f20ba7649740a19e45f538b872ac
dhirajapp <dhirajkumar31298@gmail.com>
2023-08-17 12:20:58 +0530

    implemented versioning restAPI

---
+++ restful-web-services/src/main/java/com/dhirajapp/restfulwebservices/versioning/Name.java
@@ -0,0 +1,30 @@
+package com.dhirajapp.restfulwebservices.versioning;
+
+public class Name {
+	private String firstName;
+	 private String lastName;
+	
+	 public Name() {
+	 }
+	public Name(String firstName2, String lastName2) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	 public String getFirstName() {
+		 return firstName;
+		 }
+		 public void setFirstName(String firstName) {
+		 this.firstName = firstName;
+		 }
+		 public String getLastName() {
+		 return lastName;
+		 }
+		 public void setLastName(String lastName) {
+		 this.lastName = lastName;
+		 }
+
+	
+	 
+	 
+}
---
+++ restful-web-services/src/main/java/com/dhirajapp/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,19 @@
+package com.dhirajapp.restfulwebservices.versioning;
+
+public class PersonV1 {
+	private String name;
+
+	public PersonV1(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonV1 [name=" + name + "]";
+	}
+
+}
