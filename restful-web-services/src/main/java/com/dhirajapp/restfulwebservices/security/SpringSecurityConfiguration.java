56e05c508d00cb8080cc0ad88f081899913715fa
dhirajapp <dhirajkumar31298@gmail.com>
2023-08-21 19:03:43 +0530

    implemented spring security configuration for basic authentication

---
+++ restful-web-services/src/main/java/com/dhirajapp/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.dhirajapp.restfulwebservices.security;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+   
+@Bean
+   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	   
+	//spring security  -> filterChain
+	
+	
+	
+	// All request should be authenticated 
+	   http.authorizeHttpRequests(
+			   auth->auth.anyRequest().authenticated()
+			   );
+	   // if a request is not authenticated , a web page shown 
+	   http.httpBasic(withDefaults());
+	   
+	   // csrf -> post ,put
+	   http.csrf().disable();
+	   
+	    return http.build();
+   }
+}
